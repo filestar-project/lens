17f72a8e8aae117fe50297e357ea76b1e944f1a0
cocochen <chenshuai@6block.com>
2021-09-24 15:04:55 +0800

    temp env config

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@ import * as Environment from "~/node_common/environment";
 const developmentConfig = {
   client: "pg",
   connection: {
-    ssl: true,
+    ssl: false,
     port: 5432,
     host: Environment.POSTGRES_HOSTNAME,
     database: Environment.POSTGRES_DATABASE,
@@ -15,7 +15,7 @@ const developmentConfig = {
 const productionConfig = {
   client: "pg",
   connection: {
-    ssl: true,
+    ssl: false,
     port: 5432,
     host: Environment.POSTGRES_HOSTNAME,
     database: Environment.POSTGRES_DATABASE,
--- node_common/environment.js
+++ node_common/environment.js
@@ -1,13 +1,13 @@
 export const NODE_ENV = process.env.NODE_ENV || "development";
 export const IS_PRODUCTION = NODE_ENV === "production";
 export const PORT = process.env.PORT || 1313;
-export const SOURCE = process.env.SOURCE;
+export const SOURCE = process.env.SOURCE || 'lens';
 
 // NOTE(jim):
 // In production we don't use .env and manage secrets another way.
-if (!IS_PRODUCTION) {
+// if (!IS_PRODUCTION) {
   require("dotenv").config();
-}
+// }
 
 export const POSTGRES_ADMIN_PASSWORD = process.env.POSTGRES_ADMIN_PASSWORD;
 export const POSTGRES_ADMIN_USERNAME = process.env.POSTGRES_ADMIN_USERNAME;
